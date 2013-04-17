1b723789689e3240bab5d10f621cc5bfb7d47494
Sergey Storchay <r8@r8.com.ua>
2013-04-17 09:06:56 +0300

    Remove pprint import

--- wp2github.py
+++ wp2github.py
@@ -21,7 +21,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 import argparse, string, re, sys
-from pprint import pprint
 from collections import OrderedDict
 
 def parse_arguments():
