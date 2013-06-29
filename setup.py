73cb72d0709bce0769ef91f689ecf9a5987bb7dc
Sergey Storchay <r8@r8.com.ua>
2013-06-29 16:07:04 +0300

    Update module configuration

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from wp2github import _version
+
+# Get the requirements from the pip requirements file
+requirements = []
+
+with open("requirements.txt") as f:
+    for l in f:
+        l = l.strip()
+        if l:
+            requirements.append(l)
 
 setup(name="wp2github",
-            version="1.0.1",
-            description="Convert WordPress plugin readme file to GitHub Flavored Markdown",
-            long_description=open("README.md").read(),
-            author="Sergey Storchay",
-            author_email="r8@r8.com.ua",
-            url="https://github.com/r8/wp2github.py",
-            license="MIT",
-            packages=["wp2github"],
-            entry_points = { "console_scripts" : [ "wp2github = wp2github.wp2github:main"]},
-           )
+      version=_version.__version__,
+      description="Convert WordPress plugin readme file to GitHub Flavored Markdown",
+      long_description=open("README.md").read(),
+      author="Sergey Storchay",
+      author_email="r8@r8.com.ua",
+      url="https://github.com/r8/wp2github.py",
+      license="MIT",
+      packages=["wp2github"],
+      entry_points={"console_scripts": ["wp2github = wp2github.wp2github:main"]},
+      install_requires=requirements
+)
