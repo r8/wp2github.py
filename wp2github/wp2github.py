6671d6adfcc3ec6443e84cc5b7bdd2219c125c63
Sergey Storchay <r8@r8.com.ua>
2013-06-30 13:45:59 +0300

    Convert module from procedural to OOP style

--- wp2github/wp2github.py
+++ wp2github/wp2github.py
@@ -29,137 +29,130 @@ from collections import OrderedDict
 import config
 
 
-def parse_arguments():
-    p = argparse.ArgumentParser(
-        description=config.__description__,
-        version='%s %s' % (config.__title__, config.__version__))
+class Wp2github:
+    def parse_arguments(self):
+        p = argparse.ArgumentParser(
+            description=config.__description__,
+            version='%s %s' % (config.__title__, config.__version__))
 
-    p.add_argument('--source', '-s', default='readme.txt',
-                   help='Source file path (default: readme.txt)')
-    p.add_argument('--target', '-t', default='README.md',
-                   help='Destination file path (default: README.md)')
+        p.add_argument('--source', '-s', default='readme.txt',
+                       help='Source file path (default: readme.txt)')
+        p.add_argument('--target', '-t', default='README.md',
+                       help='Destination file path (default: README.md)')
 
-    return p.parse_args()
+        return p.parse_args()
 
+    def convert(self, source, target):
+        try:
+            input_file = open(source, 'r')
+        except IOError:
+            print "Error opening source file: %s" % source
+            sys.exit(1)
 
-def convert(source, target):
-    try:
-        input_file = open(source, 'r')
-    except IOError:
-        print "Error opening source file: %s" % source
-        sys.exit(1)
+        name, data = self.split_sections(input_file.readlines())
+        input_file.close()
 
-    name, data = split_sections(input_file.readlines())
-    input_file.close()
+        output = self.format_header(name, data['main'])
 
-    output = format_header(name, data['main'])
+        for name in data.keys():
+            output += self.format_section(name, data[name])
 
-    for name in data.keys():
-        output += format_section(name, data[name])
+        try:
+            output_file = open(target, 'w')
+        except IOError:
+            print "Error opening target file: %s" % target
+            sys.exit(1)
 
-    try:
-        output_file = open(target, 'w')
-    except IOError:
-        print "Error opening target file: %s" % target
-        sys.exit(1)
+        output_file.write(output)
+        output_file.close()
 
-    output_file.write(output)
-    output_file.close()
+    def split_sections(self, data):
+        name = data.pop(0).strip("= \r\n")
 
+        output = OrderedDict()
+        section_name = 'main'
+        section_body = ''
 
-def split_sections(data):
-    name = data.pop(0).strip("= \r\n")
+        for line in data:
+            section_match = re.match('^==[^=]*?==', line)
+            if section_match:
+                output[section_name] = section_body.rstrip(" \r\n")
+                section_body = ''
+                section_name = line.strip("= \r\n").lower()
+            else:
+                section_body += line.replace("\r\n", "\n").replace("\r", "\n")
 
-    output = OrderedDict()
-    section_name = 'main'
-    section_body = ''
+        if section_body:
+            output[section_name] = section_body
 
-    for line in data:
-        section_match = re.match('^==[^=]*?==', line)
-        if section_match:
-            output[section_name] = section_body.rstrip(" \r\n")
-            section_body = ''
-            section_name = line.strip("= \r\n").lower()
-        else:
-            section_body += line.replace("\r\n", "\n").replace("\r", "\n")
+        return name, output
 
-    if section_body:
-        output[section_name] = section_body
+    def format_header(self, name, data):
+        output = "# %s\n\n" % name
 
-    return name, output
+        list_item = True
+        for line in data.split("\n"):
+            if not line.strip():
+                list_item = False
 
-
-def format_header(name, data):
-    output = "# %s\n\n" % name
-
-    list_item = True
-    for line in data.split("\n"):
-        if not line.strip():
-            list_item = False
-
-        if list_item:
-            output += "* "
-        output += "%s\n" % line
-
-    return output
-
-
-def format_section(name, data):
-    if name == 'main':
-        return ''
-
-    output = "\n## %s\n" % string.capwords(name)
-
-    if name == 'screenshots':
-        return output + format_screenshots(data)
-
-    for line in data.split("\n"):
-        output += "%s\n" % format_line(line)
-
-    return output
-
-
-def format_line(line):
-    line = re.sub(r'^=([^=]*?)=', r'###\1', line)
-    return line
-
-
-def format_screenshots(data):
-    output = ''
-    i = 0
-
-    for line in data.split("\n"):
-        if not line.strip():
+            if list_item:
+                output += "* "
             output += "%s\n" % line
-        else:
-            i += 1
-            text = re.sub(r'^[0-9]+\.\s+(.*?)', r'\1', line)
-            link = format_screenshot_link(i)
-            output += "![%s](%s \"%s\")\n\n" % (text, link, text)
 
-    return output
+        return output
 
+    def format_section(self, name, data):
+        if name == 'main':
+            return ''
 
-def format_screenshot_link(i):
-    link = "http://wordpress.org/plugins/pluginname/screenshot-%i.png" % i
-    return link
+        output = "\n## %s\n" % string.capwords(name)
 
+        if name == 'screenshots':
+            return output + self.format_screenshots(data)
 
-def image_exists(site, path):
-    conn = httplib.HTTPConnection(site)
-    conn.request('HEAD', path)
-    response = conn.getresponse()
-    conn.close()
-    return response.status == 200
+        for line in data.split("\n"):
+            output += "%s\n" % self.format_line(line)
 
+        return output
 
-def main():
-    arguments = parse_arguments()
-    if not arguments:
-        sys.exit(2)
+    def format_line(self, line):
+        line = re.sub(r'^=([^=]*?)=', r'###\1', line)
+        return line
 
-    convert(arguments.source, arguments.target)
+    def format_screenshots(self, data):
+        output = ''
+        i = 0
+
+        for line in data.split("\n"):
+            if not line.strip():
+                output += "%s\n" % line
+            else:
+                i += 1
+                text = re.sub(r'^[0-9]+\.\s+(.*?)', r'\1', line)
+                link = self.format_screenshot_link(i)
+                output += "![%s](%s \"%s\")\n\n" % (text, link, text)
+
+        return output
+
+    def format_screenshot_link(self, i):
+        link = "http://wordpress.org/plugins/pluginname/screenshot-%i.png" % i
+        return link
+
+    def image_exists(self, site, path):
+        conn = httplib.HTTPConnection(site)
+        conn.request('HEAD', path)
+        response = conn.getresponse()
+        conn.close()
+        return response.status == 200
+
+    def main(self):
+        arguments = self.parse_arguments()
+        if not arguments:
+            sys.exit(2)
+
+        self.convert(arguments.source, arguments.target)
 
 
 if __name__ == '__main__':
-    main()
+    wp2github = Wp2github()
+    wp2github.main()
